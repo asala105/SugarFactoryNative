ff90b87d485fc102cc46b5f0d32fc590498f3927
Mouhamad47 <74287155+Mouhamad47@users.noreply.github.com>
2021-10-06 23:38:46 +0300

    Small edits for the edit profile page

--- screens/chatScreen.js
+++ screens/chatScreen.js
@@ -1,14 +1,52 @@
-import React from 'react';
+import React ,{useState,useEffect, useCallback}from 'react';
 import { StyleSheet, Text, View, ScrollView, ImageBackground, Dimensions, Image, Item, TextInput, Button, Pressable } from 'react-native';
-
+import { Bubble, GiftedChat } from 'react-native-gifted-chat'
 
 
 
 export default function ChatScreen(){
+    
+    const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ])
+  }, [])
+
+
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+  }, [])
+
+  const renderBubble =(props)=>{
+      <Bubble 
+      {...props}
+      wrapperStyle={{
+          right :{
+              backgroundColor : '#8C0C33'
+          }
+      }} />
+  }
+    
     return (
-       <View>
-           <Text>Hello from Chat screen</Text>
-       </View>
+        <GiftedChat
+        messages={messages}
+        onSend={messages => onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+        
+      />
     );
 
 
